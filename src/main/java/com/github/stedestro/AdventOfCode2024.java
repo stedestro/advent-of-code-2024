89788fc3a4c784bab05dfefa842ed70ea3167d87
Steven Destro <8580017+stedestro@users.noreply.github.com>
2024-12-01 11:17:29 +0100

    initial commit

---
+++ src/main/java/com/github/stedestro/AdventOfCode2024.java
@@ -0,0 +1,7 @@
+package com.github.stedestro;
+
+public class AdventOfCode2024 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/github/stedestro/AdventOfCode2024Test.java
@@ -0,0 +1,19 @@
+package com.github.stedestro;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AdventOfCode2024Test {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
